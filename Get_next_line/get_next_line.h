5ce36e8739b645341ff2fca0151ccb9ca96674ca
infleir <anthbraybur@gmail.com>
2024-07-30 12:03:41 +0200

    eliminacion de un directorio y empezando con Get_next_line

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aburga-g < aburga-g@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 21:46:13 by aburga-g          #+#    #+#             */
+/*   Updated: 2024/07/25 12:28:45 by aburga-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	get_next_line_utils(int fd);
+
+#endif
