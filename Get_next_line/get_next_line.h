973514c4685666bb96e828d88d7189da2d4c7a92
infleir <anthbraybur@gmail.com>
2024-08-26 13:32:05 +0200

    funciones añadidas a la libreria

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aburga-g < aburga-g@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:46:13 by aburga-g          #+#    #+#             */
-/*   Updated: 2024/07/30 14:58:27 by aburga-g         ###   ########.fr       */
+/*   Updated: 2024/08/26 12:50:01 by aburga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
+# include <fcntl.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
 
 char	*get_next_line(int fd);
-char	get_next_line_utils(int fd);
+
+/* auxiliary functions*/
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
