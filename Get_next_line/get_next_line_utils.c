9690ae6d2a00192d6de65063676f869cd17f7f62
infleir <anthbraybur@gmail.com>
2024-08-26 13:33:10 +0200

    funciones nuevas creadas

--- Get_next_line/get_next_line_utils.c
+++ Get_next_line/get_next_line_utils.c
@@ -6,12 +6,11 @@
 /*   By: aburga-g < aburga-g@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:46:31 by aburga-g          #+#    #+#             */
-/*   Updated: 2024/07/30 15:21:11 by aburga-g         ###   ########.fr       */
+/*   Updated: 2024/08/26 13:12:40 by aburga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
 char	*ft_strchr(const char *s, int c)
 {
@@ -19,22 +18,70 @@ char	*ft_strchr(const char *s, int c)
 
 	i = 0;
 	while (s[i] != c && s[i] != '\0')
-	{
-		if ()
 		i++;
-	}
-		
-		
-	if (s[i] == c)
-		return (s);
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
 	return (NULL);
 }
-int	main(void)
-{
-	char	str[] = "hola-cara cola";
-	char	*c;
 
-	c = ft_strchr(str, '-');
-	printf ("%s", c);
-	return (0);
+size_t	ft_strlen(const char *str)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	str = malloc ((ft_strlen(s1) + ft_strlen(s2) +1) * sizeof(char));
+	if (!str || !s1 || !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+	unsigned int	i;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*r;
+	size_t	e;
+
+	e = count * size;
+	r = malloc(e);
+	if (!r)
+		return (NULL);
+	ft_bzero(r, e);
+	return (r);
 }
