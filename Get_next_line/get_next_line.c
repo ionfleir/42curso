a8ea2d8daed3259764fc9351a8461cd83e1cf45c
infleir <anthbraybur@gmail.com>
2024-09-13 10:56:27 +0200

    funciones get_next_line completos

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,10 +6,142 @@
 /*   By: aburga-g < aburga-g@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:46:25 by aburga-g          #+#    #+#             */
-/*   Updated: 2024/07/18 14:12:24 by aburga-g         ###   ########.fr       */
+/*   Updated: 2024/09/10 10:17:28 by aburga-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd);
+char	*ft_free(char *buffer, char *buf)
+{
+	char	*buffer2;
+
+	buffer2 = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (buffer2);
+}
+
+static char	*buffer_copy(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	line = 0;
+	if (buffer[i] == '\0')
+		return (0);
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\n')
+		line = ft_calloc((i + 2), sizeof(char));
+	else
+		line = ft_calloc((i + 1), sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i] = '\n';
+	return (line);
+}
+
+char	*buffer_rest(char *buffer)
+{
+	char	*temp;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\0')
+	{
+		free(buffer);
+		return (NULL);
+	}
+	temp = ft_calloc(ft_strlen(buffer) - i + 1, sizeof(char));
+	i++;
+	while (buffer[i] != '\0')
+	{
+		temp[j] = buffer[i];
+		i++;
+		j++;
+	}
+	free(buffer);
+	return (temp);
+}
+
+static char	*read_file(char *buffer, int fd)
+{
+	char	*temp;
+	int		i;
+
+	if (!buffer)
+		buffer = ft_calloc(1, 1);
+	temp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	i = 1;
+	while (i > 0)
+	{
+		i = read(fd, temp, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(temp);
+			free(buffer);
+			return (0);
+		}
+		temp[i] = '\0';
+		buffer = ft_free(buffer, temp);
+		if (ft_strchr(temp, '\n'))
+			break ;
+	}
+	free(temp);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(buffer);
+		buffer = NULL;
+		return (0);
+	}
+	buffer = read_file(buffer, fd);
+	if (buffer == 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	line = buffer_copy(buffer);
+	buffer = buffer_rest(buffer);
+	return (line);
+}
+
+/*#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	i = 1;
+	fd = open("test.txt", O_RDONLY);
+	while (i > 0)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		printf("%s", line);
+		free (line);
+	}
+	close(fd);
+	return (0);
+}*/
