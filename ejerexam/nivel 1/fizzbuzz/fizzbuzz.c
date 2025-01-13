aeeb770dcf9064d241a304c2c777eeba92088bdd
infleir <anthbraybur@gmail.com>
2025-01-13 13:46:59 +0100

    Funcion fizzbuzz

--- ejerexam/nivel 1/fizzbuzz/fizzbuzz.c
+++ ejerexam/nivel 1/fizzbuzz/fizzbuzz.c
@@ -1,1 +1,29 @@
 #include <unistd.h>
+
+void	fizzbuzz(int num)
+{
+	char	str[10] = "0123456789";
+
+	if (num > 9)
+		fizzbuzz(num / 10);
+	write (1, &str[num % 10], 1);
+}
+
+int	main(void)
+{
+	int	i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			write (1, "fizzbuzz", 8);
+		else if (i % 3 == 0)
+			write (1, "fizz", 4);
+		else if (i % 5 == 0)
+			write (1, "buzz", 4);
+		else
+			fizzbuzz(i);
+		i++;
+		write (1, "\n", 1);
+	}
+}
