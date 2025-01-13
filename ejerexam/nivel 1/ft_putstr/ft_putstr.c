ef015617ae93f465b9ff4504cee77e5ae2c0695b
infleir <anthbraybur@gmail.com>
2025-01-13 13:47:30 +0100

    Funcion ft_putstr

--- ejerexam/nivel 1/ft_putstr/ft_putstr.c
+++ ejerexam/nivel 1/ft_putstr/ft_putstr.c
@@ -1,27 +1,37 @@
 #include <unistd.h>
 
-/*void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}*/
-
-void	ft_putstr(char *str)
+/*void	ft_putstr(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while(str[i])
 	{
-		/*ft_putchar(str[i]);*/
-		write (1, &str[i], 1);
+		write(1, &str[i], 1);
 		i++;
 	}
-}
+}*/
 
-int	main(void)
+/*int	main(void)
 {
 	char	str[] = "hola";
 
 	ft_putstr(str);
+	return (0);
+}*/
+
+int	main(int argc, char **argv)
+{
+	int	i = 0;
+
+	if (argc == 2)
+	{
+		while(argv[1][i])
+		{
+			write (1, &argv[1][i], 1);
+			i++;
+		}
+		write (1, "\n", 1);
+	}
 	return (0);
 }
