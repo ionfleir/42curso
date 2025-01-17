347d54d9176b92b42a169d558bb0f61ed7300527
tony <anthbraybur@gemail.com>
2025-01-17 21:21:31 +0100

    Ejercicios nivel 1

--- ejerexam/nivel 1/ulstr/ulstr.c
+++ ejerexam/nivel 1/ulstr/ulstr.c
@@ -7,20 +7,19 @@ void	ulstr(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'a')
 			str[i] -= 32;
-		else if (str[i] >= 'A' && str[i] <= 'Z')
+		else if (str[i] >= 'A')
 			str[i] += 32;
 		write (1, &str[i], 1);
 		i++;
 	}
-	write (1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		ulstr(argv[1]);
-	else
-		write (1, "\n", 1);
+	write(1, "\n", 1);
+	return (0);
 }
