fa40c53bc755ba61cdc4e57b7cf5a051d0255f40
infleir <anthbraybur@gmail.com>
2025-01-24 10:06:09 +0100

    modificados

--- ejerexam/nivel 1/ulstr/ulstr.c
+++ ejerexam/nivel 1/ulstr/ulstr.c
@@ -7,9 +7,9 @@ void	ulstr(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] >= 'a')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		else if (str[i] >= 'A')
+		else if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
 		write (1, &str[i], 1);
 		i++;
