2802cf62f6dc9ff8bd8670bfb6dddf3bd2545035
infleir <anthbraybur@gmail.com>
2025-02-07 13:28:14 +0100

    nuevo

--- ejerexam/nivel 2/inter/inter.c
+++ ejerexam/nivel 2/inter/inter.c
@@ -8,17 +8,51 @@ int	main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		while (argv[1][i] && argv[2][j])
+		while (argv[1][i])
 		{
-			if (argv[1][i] == argv[2][j])
+			j = 0;
+			while (argv[2][j])
 			{
-				argv[1][i++] = argv[2][j++];
-				write(1, &argv[1][i], 1);
+				if (argv[1][i] == argv[2][j] && !table[(int)argv[2][j]])
+				{
+					table[(int)argv[2][j]] = 1;
+					write(1, &argv[2][j], 1);
+				}
+				j++;
 			}
 			i++;
-			j++;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+
+/*void	inter(char *s1, char *s2)
+{
+	int		j = 0;
+	int		i = 0;
+	char	table[256] = {};
+
+	while (s1[i])
+	{
+		j = 0;
+		while (s2[j])
+		{
+			if (s1[i] == s2[j] && !table[(int)s2[j]])
+			{
+				table[(int)s2[j]] = 1;
+				write (1, &s2[j], 1);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+int	main(void)
+{
+	char	s1[] = "padinto";
+	char	s2[] = "paqefwtdjetyiytjneytjoeyjnejeyj";
+
+	inter(s1, s2);
+	return (0);
+}*/
